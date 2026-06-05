b816a7f1c73c5090bf59ec472ee86ab207e9a6d5
AzucenaMejia <blazu.mejia11@gmail.com>
2026-06-05 13:11:23 -0500

    Se hacen los ultimos cambios en el proyecto y se agrega el js.

--- javascript/instrucciones.js
+++ javascript/instrucciones.js
@@ -1,2 +1,24 @@
- 
+const inputSaltos = document.getElementById("saltos");
+const botonIniciar = document.querySelector(".boton-iniciar");
+const pelota = document.querySelector(".pelota");
+const contador = document.getElementById("contador");
 
+botonIniciar.addEventListener("click", () => {
+    const saltos = parseInt(inputSaltos.value);
+
+    if (isNaN(saltos) || saltos <= 0) {
+        alert("Por favor ingresa un número válido");
+        return;
+    }
+    for (let i = 1; i <= saltos; i++) {
+        setTimeout(() => {
+            pelota.classList.add("saltar");
+
+            pelota.addEventListener("animationend", () => {
+                pelota.classList.remove("saltar");
+            }, { once: true }); 
+            contador.textContent = `Salto número ${i}`;
+        }, i * 600);
+    }
+
+});
